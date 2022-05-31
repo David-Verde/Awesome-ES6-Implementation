9c0be804fff3a1d95d4464c7ac5df71a51bc174d
Unyielding1 <david.verde.alvarez@gmail.com>
2022-05-30 19:59:16 -0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,25 @@
-module.exports = {
+{
     "env": {
-        "browser": true,
-        "es2021": true
+      "browser": true,
+      "es6": true,
+      "jest": true
     },
+    "parser": "babel-eslint",
     "parserOptions": {
-        "ecmaVersion": "latest"
+      "ecmaVersion": 2018,
+      "sourceType": "module"
     },
+    "extends": ["airbnb-base"],
     "rules": {
-    }
-}
+      "no-shadow": "off",
+      "no-param-reassign": "off",
+      "eol-last": "off",
+      "import/extensions": [ 1, {
+        "js": "always", "json": "always"
+      }]
+    },
+    "ignorePatterns": [
+      "dist/",
+      "build/"
+    ]
+  }
